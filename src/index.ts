4bf47c3ea70aa178498bdc5d6c9e026c3a0ab416
Mark Canning <markcanning22@gmail.com>
2023-04-07 18:30:19 +0100

    Added CommentCreated event

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 import {randomBytes} from 'crypto';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 app.use (bodyParser.json());
@@ -13,7 +14,7 @@ app.get('/posts/:id/comments', (req: Request, res: Response): void => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req: Request, res: Response): void => {
+app.post('/posts/:id/comments', async (req: Request, res: Response): Promise<void> => {
     const commentId: string = randomBytes(4).toString('hex');
     const { content } = req.body;
 
@@ -23,7 +24,22 @@ app.post('/posts/:id/comments', (req: Request, res: Response): void => {
 
     commentsByPostId[req.params.id] = comments;
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    });
+
     res.status(201).send(comments);
+});
+
+app.post('/events', (req: Request, res: Response): void => {
+    console.log('Received event', req.body.type);
+
+    res.send({});
 });
 
 app.listen(4001, (): void => {
